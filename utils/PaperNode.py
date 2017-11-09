9cda4abaabc6297d009e051df170d1fe76d189c2
DongSky <dongsky@live.cn>
2017-11-09 10:35:18 +0800

    PDF utils first upload, create PDF_Manage class and initialize PaperNode class

---
+++ utils/PaperNode.py
@@ -0,0 +1,9 @@
+import os
+
+class PaperNode:
+    def __init__(self, title, author, time, tags, filePath):
+        self.title = title
+        self.author = author
+        self.time = time
+        self.tags = tags
+        self.filePath = filePath
