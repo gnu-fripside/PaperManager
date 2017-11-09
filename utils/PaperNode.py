58d1d1b4f4cb9ca449155cecc0b257ece2ad2657
DongSky <dongsky@live.cn>
2017-11-09 19:23:24 +0800

    add gitignore

--- utils/PaperNode.py
+++ utils/PaperNode.py
@@ -1,9 +1,15 @@
 import os
 
 class PaperNode:
-    def __init__(self, title, author, time, tags, filePath):
+    def __init__(self, title, author, time, tags, source, filePath):
         self.title = title
         self.author = author
         self.time = time
         self.tags = tags
+        self.source = source
+        self.notes = None
         self.filePath = filePath
+    def AddNote(self):
+        pass
+    def Pack(self):
+        pass
