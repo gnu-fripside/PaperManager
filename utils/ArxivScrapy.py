224241603574fb12b4320fcdcb2d578b7ce21a8c
DongSky <dongsky@live.cn>
2017-11-10 19:18:54 +0800

    ArxivScrapy debug complete

--- utils/ArxivScrapy.py
+++ utils/ArxivScrapy.py
@@ -11,11 +11,15 @@ def paperDown(arxivID, paperDir):
     downloadLink = "https://arxiv.org/pdf/" + str(arxivID) + ".pdf"
     request = urllib.request.Request(downloadLink)
     request.add_header("User-Agent", constUA)
-    get = urllib.request.urlopen(request)
+    get = urllib.request.urlopen(request).read()
     sha1obj = hashlib.sha1()
     sha1obj.update(get)
     hash = str(sha1obj.hexdigest())
+    if not os.path.exists(paperDir):
+        os.makedirs(paperDir)
     with open(os.path.join(paperDir, hash+".pdf"), "wb") as f:
         f.write(get)
         f.close()
     return os.path.join(paperDir, hash+".pdf")
+# if __name__ == "__main__":
+#     print(paperDown("1407.0028","paper"))
