751777e9f1053344c97284e722e327bc3abf8acc
DongSky <dongsky@live.cn>
2017-11-10 12:32:09 +0800

    add some comment

--- utils/PaperNode.py
+++ utils/PaperNode.py
@@ -3,14 +3,38 @@ import hashlib
 import shutil
 import zipfile
 """
-str title: paper's title
-str time: 
+class PaperNode:
+    str userid: the symbol of user
+    str title: paper's title
+    str publishtime: paper's publish time
+    str addtime: paper's add time
+    list(str) tags: tags of this paper, edited by each user, from root to leaf
+    str source: the conference or periodical of this paper
+    dict notes: notes add by user
+    str filePath: the path of origin file
+    str hash: sha1 value of the origin file
+    
+    method AddNote
+    
+    method toDict():
+        return: dict dictPaperNode: paper info in dict type
+    
+    method Pack(userid, tempDir, outputDir):
+        not complete!!!
+        :parameter
+            str userid: the symbol of user
+            str tempDir: path of temp directory
+            str outputDir: path of output directory, store the formatted file
+        :return
+            str outputPath: the path of formatted file
 """
 class PaperNode:
-    def __init__(self, title, author, time, tags, source, filePath):
+    def __init__(self, userid, title, author, publishtime, addtime, tags, source, filePath):
+        self.userid = userid
         self.title = title
         self.author = author
-        self.time = time
+        self.publishtime = publishtime
+        self.addtime = addtime
         self.tags = tags
         self.source = source
         self.notes = {}
@@ -28,7 +52,8 @@ class PaperNode:
         dictPaperNode = {}
         dictPaperNode["title"] = self.title
         dictPaperNode["author"] = self.author
-        dictPaperNode["time"] = str(self.time)
+        dictPaperNode["publishtime"] = str(self.time)
+        dictPaperNode["addtime"] = str(self.addtime)
         dictPaperNode["tags"] = {}
         for i in range(len(self.tags)):
             dictPaperNode["tags"][str(i+1)] = self.tags[i]
@@ -41,15 +66,17 @@ class PaperNode:
     def Pack(self, userid, tempDir, outputDir):
         path = os.path.join(tempDir, str(userid)+"_"+self.hash)
         notePath = os.path.join(path, "noteFile")
+        logPath = os.path.join(path, "logFile")
         os.makedirs(path)
         os.makedirs(notePath)
         shutil.copy(self.filePath, os.path.join(tempDir, self.hash+".pdf"))
         noteKeys = self.notes.keys()
         for i in range(len(noteKeys)):
             shutil.copy(self.notes[noteKeys[i]], os.path.join())
-        with zipfile.ZipFile(os.path.join(tempDir, str(userid)+"_"+self.hash+".zip"),"w",zipfile.ZIP_DEFLATED) as f:
+        with zipfile.ZipFile(os.path.join(outputDir, str(userid)+"_"+self.hash+".zip"),"w",zipfile.ZIP_DEFLATED) as f:
             for dirPath, dirNames, fileNames in os.walk(path):
                 for filename in fileNames:
                     f.write(os.path.join(dirPath,filename))
             f.close()
-        return os.path.join(tempDir, str(userid)+"_"+self.hash+".zip")
+        shutil.rmtree(path)
+        return os.path.join(outputDir, str(userid)+"_"+self.hash+".zip")
--- utils/util.py
+++ utils/util.py
@@ -3,7 +3,7 @@ from utils.TagTree import TagTree
 from utils.ArxivScrapy import paperDown
 from utils.PaperNode import PaperNode
 
-def AddPaper():
+def AddPaper(userid, title, author, time, tags, source, filePath):
     pass
 
 def initializeTagTree(userid, filePath):
@@ -12,8 +12,13 @@ def initializeTagTree(userid, filePath):
         f.write(str(rootTag.toDict()))
         f.close()
     return rootTag
-
-def AddTag(tag, parentTag):
+"""
+function AddTag(userid, tag, parentTag)
+    str userid: the symbol of TagTree
+    str tag: tag needs to be added
+    list parentTag: parent tags, from root to direct parent of new tag
+"""
+def AddTag(userid, tag, parentTag):
     pass
 
 
