89c88d5af138f743049a973232ba87d2aa339f88
makeztc <makeztc@gmail.com>
2017-11-12 14:51:35 +0800

    user register and login

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Book(models.Model):
     book_name = models.CharField(max_length=64)
     add_time = models.DateTimeField(auto_now_add=True)
@@ -7,4 +8,8 @@ class Book(models.Model):
     def __unicode__(self):
         return self.book_name
 
-# Create your models here.
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=50)
+    user_email = models.CharField(max_length=50)
