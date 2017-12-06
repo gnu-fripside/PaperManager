b6043fe3838a8c2a0a40840e3bd73f9e7361a33c
makeztc <makeztc@gmail.com>
2017-12-06 15:32:19 +0800

    finish a part of the backend

--- backend/views.py
+++ backend/views.py
@@ -20,8 +20,8 @@ def user_register(request):
         response['error_num'] = 1
         response['msg'] = 'The user has been registered!'
     else:
-        root = ClassificationTree.objects.create(name='root')
-        user = Users.objects.create(username=username, password=password, email=email, classification_tree_root=root)
+        user = Users.objects.create(username=username, password=password,
+                                    email=email, classification_tree_root='root')
         response['error_num'] = 0
         response['msg'] = 'success'
     return JsonResponse(response)
@@ -76,8 +76,8 @@ def add_classification(request):
     username = request.POST['username']
     father_name = request.POST['father_name']
     new_name = request.POST['name']
-    father = ClassificationTree.objects.filter(name=father_name, username=username)
-    exist = ClassificationTree.objects.filter(name=new_name, username=username)
+    father = ClassificationTree.objects.filter(username=username, name=father_name)
+    exist = ClassificationTree.objects.filter(username=username, name=new_name)
     if father is None:
         response['error_num'] = 1
         response['msg'] = 'the father node does not exist'
@@ -86,8 +86,8 @@ def add_classification(request):
         response['msg'] = 'the node name had existed'
     else:
         node = ClassificationTree.objects.create(username=username,
-                                                 ClassificationTree=father[0],
-                                                 name=new_name)
+                                                 name=new_name,
+                                                 father=father_name)
         response['error_num'] = 0
         response['msg'] = 'success'
     res = JsonResponse(response)
@@ -110,12 +110,11 @@ def add_paper(request):
     url = request.POST['url']
     hash_code = request.POST['hash_code']
     node_name = request.POST['node_name']
-    node = ClassificationTree.objects.filter(username=username, name=node_name)
     paper = Paper.objects.create(username=username, title=title,
                                  publish_time=publish_time,
                                  add_time=add_time, source=source,
                                  url=url, hash_code=hash_code,
-                                 classification_tree_node=node[0])
+                                 classification_tree_node=node_name)
     for au in authors:
         author = Author.objects.filter(first_name=au['first_name'],
                                        last_name=au['last_name'],
@@ -132,7 +131,7 @@ def add_paper(request):
     return res
 
 
-def save_node(request):
+def save_note(request):
     """
     add and change node for a paper
     :param request: request
@@ -142,6 +141,7 @@ def save_node(request):
     username = request.POST['username']
     paper_title = request.POST['paper_title']
     paper_page = request.POST['paper_page']
+    note_content = request.POST['content']
     exist = Note.objects.filter(username=username, paper_title=paper_title, paper_page=paper_page)
     if exist:
         exist[0].delete()
@@ -150,7 +150,8 @@ def save_node(request):
         response['msg'] = 'add note successfully'
     note = Note.objects.create(username=username,
                                paper_title=paper_title,
-                               paper_page=paper_page)
+                               paper_page=paper_page,
+                               content=note_content)
     response['error_num'] = 0
     res = JsonResponse(response)
     return res
@@ -166,12 +167,12 @@ def show_paper_detail(request):
     title = request.POST['title']
     username = request.POST['username']
     response['title'] = title
-    paper = Paper.objects.filter(title=title, username=username)
+    paper = Paper.objects.filter(username=username, title=title)
     response['publish_time'] = paper[0].publish_time
     response['add_time'] = paper[0].add_time
     response['source'] = paper[0].source
     response['url'] = paper[0].url
-    res = JsonResponse[response]
+    res = JsonResponse(response)
     return res
 
 
@@ -184,12 +185,15 @@ def read_paper(request):
     response = {}
     username = request['username']
     title = request['title']
-    page = request['page']
-    paper = Paper.objects.filter(username=username, title=title)
-    note = Note.objects.filter(username=username, paper_title=title, paper_page=page)
+    notes = Note.objects.filter(username=username, paper_title=title)
+    note = []
+    for note_ex in notes:
+        tmp = {'page': note_ex.paper_page, 'content': note_ex.content}
+        note.append(tmp)
     response['path'] = username+'/'+title
-    response['note'] = note[0].content
-    res = JsonResponse[response]
+    response['note'] = note
+    res = JsonResponse(response)
+    return res
 
 
 def show_paper_of_the_node(request):
@@ -211,6 +215,26 @@ def show_paper_of_the_node(request):
     return res
 
 
+def find_son(username, node):
+    """
+    find the son node name of 'node'
+    :param username: username
+    :param node: the node name
+    :return: the son list
+    """
+    son_node = ClassificationTree.objects.filter(username=username, father=node)
+    if son_node:
+        son = []
+        for son_node_ex in son_node:
+            son_node_ex = son_node_ex.name
+            tmp = {'name': son_node_ex, 'son': []}
+            tmp['son'] = find_son(username, tmp['name'])
+            son.append(tmp)
+        return son
+    else:
+        return []
+
+
 def show_classification_tree(request):
     """
     show the classification tree
@@ -220,15 +244,45 @@ def show_classification_tree(request):
     username = request.POST['username']
     user = Users.objects.get(username=username)
     root = user.classification_tree_root
+    node = {'name': root, 'son': find_son(username, root)}
+    response = {'node': node}
+    res = JsonResponse(response)
+    return res
+
+
+def find_son_paper(username, node_name):
+    """
+    find the papers in the son node of the node_name
+    :param username: username
+    :param node_name: the name of the node
+    :return: the title of the papers
+    """
+    sons = ClassificationTree.objects.filter(username=username, father=node_name)
+    paper_title = []
+    if sons:
+        for sons_ex in sons:
+            papers = Paper.objects.filter(username=username, classification_tree_node=sons_ex.name)
+            for paper in papers:
+                paper_title.append(paper.title)
+            paper_title.extend(find_son_paper(username, sons_ex.name))
+    return paper_title
 
 
 def show_paper_of_the_subtree(request):
     """
     show the paper list of the subtree
     :param request: request
-    :return: response
+    :return: the paper title in the subtree
     """
-
+    username = request.POST['username']
+    node_name = request.POST['node_name']
+    papers = Paper.objects.filter(username=username, classification_tree_node=node_name)
+    papers_title = []
+    if papers:
+        for paper in papers:
+            papers_title.append(paper.title)
+    papers_title.extend(find_son_paper(username, node_name))
+    return papers_title
 
 
 def getTagList(request):
