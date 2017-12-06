38512928ddc5c47309d366af365da04029c55c7d
lkgv <lkgv@foxmail.com>
2017-12-06 10:22:30 +0800

    some changes

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -2,10 +2,18 @@
   <div class="index">
     <h1>Paper Manager</h1>
     <p>
-      <router-link to="/login">login</router-link>
-      <router-link to="/register">register</router-link>
-      <router-link to="/showPdf">show pdf</router-link>
-      <router-link to="/tag">tag tree</router-link>
+      <router-link to="/login">
+        <el-button>login</el-button>
+      </router-link>
+      <router-link to="/register">
+        <el-button>register</el-button>
+      </router-link>
+      <router-link to="/showPdf">
+        <el-button>show pdf</el-button>
+      </router-link>
+      <router-link to="/tag">
+        <el-button>tag tree</el-button>
+      </router-link>
     </p>
   </div>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 // import CPdf from './components/CPdf'
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
