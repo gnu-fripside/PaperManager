73e17d01703b5f7f543aaaeeab44179c1033bc5b
lkgv <lkgv@foxmail.com>
2017-12-07 16:00:25 +0800

    some changes in index and App

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -50,28 +50,28 @@
 
 <script>
   // import vue-router from 'vue-router'
- 
+
   export default {
     name: 'index',
   }
 </script>
 
 <style>
-      body{margin: 0;}
-    #app {
-      min-width: 1200px;
-      margin: 0 auto;
-      font-family: "Helvetica Neue","PingFang SC",Arial,sans-serif;
-    }
-    /* 头部导航 */
-    header{z-index: 1000;min-width: 1200px;transition: all 0.5s ease;  border-top: solid 4px #3091F2;  background-color: #fff;  box-shadow: 0 2px 4px 0 rgba(0,0,0,.12),0 0 6px 0 rgba(0,0,0,.04);  }
-    header.header-fixed{position: fixed;top: 0;left: 0;right: 0;}
-    header .el-menu-demo{padding-left: 300px!important;}
+  body{margin: 0;}
+  #app {
+    min-width: 1200px;
+    margin: 0 auto;
+    font-family: "Helvetica Neue","PingFang SC",Arial,sans-serif;
+  }
+  /* 头部导航 */
+  header{z-index: 1000;min-width: 1200px;transition: all 0.5s ease;  border-top: solid 4px #3091F2;  background-color: #fff;  box-shadow: 0 2px 4px 0 rgba(0,0,0,.12),0 0 6px 0 rgba(0,0,0,.04);  }
+  header.header-fixed{position: fixed;top: 0;left: 0;right: 0;}
+  header .el-menu-demo{padding-left: 300px!important;}
 
-    /* 主内容区 */
-      main{    display: -webkit-box;  display: -ms-flexbox;  display: flex;  min-height: 800px;  border: solid 40px #E9ECF1;  background-color: #FCFCFC;  }
-      main .main-left{text-align: center;width: 200px;float: left;}
-      main .main-right{-webkit-box-flex: 1;  -ms-flex: 1;  flex: 1;  background-color: #fff; padding: 50px 70px; }
-      main .el-menu{background-color: transparent!important;}
-    </style>
+  /* 主内容区 */
+  main{    display: -webkit-box;  display: -ms-flexbox;  display: flex;  min-height: 800px;  border: solid 40px #E9ECF1;  background-color: #FCFCFC;  }
+  main .main-left{text-align: center;width: 200px;float: left;}
+  main .main-right{-webkit-box-flex: 1;  -ms-flex: 1;  flex: 1;  background-color: #fff; padding: 50px 70px; }
+  main .el-menu{background-color: transparent!important;}
+</style>
 
